b862d26b32c54342a0445c6304f6f136c98bc06c
Samuel <samuciocan@gmail.com>
2025-02-18 11:53:28 +0100

    add pokemon name display with uppercase and lowercase transformations

--- src/pokemon-detail/pokemon-detail.component.ts
+++ src/pokemon-detail/pokemon-detail.component.ts
@@ -1,10 +1,13 @@
+import { LowerCasePipe, UpperCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-detail',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe],
   templateUrl: './pokemon-detail.component.html',
   styleUrl: './pokemon-detail.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PokemonDetailComponent { }
+export class PokemonDetailComponent {
+  pokemon = "Pikachu";
+}
