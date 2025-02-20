91de6ac9fb100693d7515fa92654645652b6caa3
Samuel <samuciocan@gmail.com>
2025-02-20 10:20:08 +0100

    add paginator component with navigation buttons and basic styles

---
+++ src/paginator/paginator.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-paginator',
+  imports: [],
+  templateUrl: './paginator.component.html',
+  styleUrl: './paginator.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PaginatorComponent {
+  @Output() nextPage = new EventEmitter<void>();
+  @Output() prevPage = new EventEmitter<void>();
+}
