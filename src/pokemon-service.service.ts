89e48ee9d01638ef7ea43cef1272981d4615fc88
Samuel <samuciocan@gmail.com>
2025-02-21 10:12:31 +0100

    add pagination support and detail retrieval in Pokémon service

--- src/pokemon-service.service.ts
+++ src/pokemon-service.service.ts
@@ -3,12 +3,20 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonServiceService {
   private http = inject(HttpClient);
 
   getPokemonList(): Observable<any> {
-    return this.http.get('https://pokeapi.co/api/v2/pokemon?limit=151');
+    return this.http.get('https://pokeapi.co/api/v2/pokemon');
+  }
+
+  changePage(url: string): Observable<any> {
+    return this.http.get(url);
+  }
+
+  getPokemonDetail(pokemonId: string): Observable<any> {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
   }
 }
