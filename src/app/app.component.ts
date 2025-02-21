042a6943b7be927bb35e601c2292a8eca1e3525f
Samuel <samuciocan@gmail.com>
2025-02-21 10:12:46 +0100

    refactor: update component references and add route for Pokémon details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// import { PokemonDetailComponent } from '../pokemon-detail/pokemon-detail.component';
+// import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
 // import { PokemonListComponent } from '../pokemon-list/pokemon-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, /* PokemonDetailComponent, PokemonListComponent */],
+  imports: [RouterOutlet, /* PokemonCardComponent, PokemonListComponent */],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,29 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '',
-        pathMatch: 'full',
-        redirectTo: 'pokemon-list',
-    },
-    {
-        path: 'pokemon-list',
-        loadComponent: () =>
-            import('../pokemon-list/pokemon-list.component').then(
-                (c) => c.PokemonListComponent,
-            ),
-    },
-    { // El doble asterisco (**) es un comodín que coincide con cualquier URL que no coincida con las rutas definidas anteriormente.
-        path: '**',
-        loadComponent: () =>
-            import('../not-found/not-found.component').then(
-                (c) => c.NotFoundComponent,
-            ),
-    }
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'pokemon-list',
+  },
+  {
+    path: 'pokemon-list',
+    loadComponent: () =>
+      import('../pokemon-list/pokemon-list.component').then(
+        (c) => c.PokemonListComponent
+      ),
+  },
+  {
+    path: 'pokemon/:pokemonId',
+    loadComponent: () =>
+      import('../pokemon/pokemon.component').then((c) => c.PokemonComponent),
+  },
+  {
+    // El doble asterisco (**) es un comodín que coincide con cualquier URL que no coincida con las rutas definidas anteriormente.
+    path: '**',
+    loadComponent: () =>
+      import('../not-found/not-found.component').then(
+        (c) => c.NotFoundComponent
+      ),
+  },
 ];
