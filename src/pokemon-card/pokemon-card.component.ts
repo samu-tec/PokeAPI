cc8039f410df437c626ff677cadb6620e8388f25
Samuel <samuciocan@gmail.com>
2025-02-21 10:11:30 +0100

    change name to Pokémon card componen

--- src/pokemon-card/pokemon-card.component.ts
+++ src/pokemon-card/pokemon-card.component.ts
@@ -2,16 +2,16 @@
 import { /* ChangeDetectionStrategy, */ Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-pokemon-detail',
+  selector: 'app-pokemon-card',
   imports: [/* LowerCasePipe, UpperCasePipe */],
-  templateUrl: './pokemon-detail.component.html',
-  styleUrl: './pokemon-detail.component.css',
+  templateUrl: './pokemon-card.component.html',
+  styleUrl: './pokemon-card.component.css',
   // changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PokemonDetailComponent {
+export class PokemonCardComponent {
   // pokemon = "Pikachu";
   @Input() pokemon: string = '';
   @Input() imagen: string = '';
-  @Input() habilities: string[] = [];
+  // @Input() habilities: string[] = [];
   @Output() clickName = new EventEmitter<string>();
 }
