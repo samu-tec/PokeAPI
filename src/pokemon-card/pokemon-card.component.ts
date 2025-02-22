58af9888ada57b791dcd7dd2dbd5700ea75a5ea5
Samuel <samuciocan@gmail.com>
2025-02-22 18:49:39 +0100

    refactor: clean up unused code and comments in PokemonCard component

--- src/pokemon-card/pokemon-card.component.ts
+++ src/pokemon-card/pokemon-card.component.ts
@@ -1,17 +1,13 @@
-// import { LowerCasePipe, UpperCasePipe } from '@angular/common';
-import { /* ChangeDetectionStrategy, */ Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-card',
-  imports: [/* LowerCasePipe, UpperCasePipe */],
+  imports: [],
   templateUrl: './pokemon-card.component.html',
   styleUrl: './pokemon-card.component.css',
-  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokemonCardComponent {
-  // pokemon = "Pikachu";
   @Input() pokemon: string = '';
   @Input() imagen: string = '';
-  // @Input() habilities: string[] = [];
   @Output() clickName = new EventEmitter<string>();
 }
