f63ea520f29d35c79009142819d0db301a0fda98
Samuel <samuciocan@gmail.com>
2025-02-22 18:49:48 +0100

    feat: add loading state to Pokemon list component with loader display

--- src/pokemon-list/pokemon-list.component.ts
+++ src/pokemon-list/pokemon-list.component.ts
@@ -1,42 +1,31 @@
-import {
-  /* ChangeDetectionStrategy, */ Component,
-  inject,
-} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { PokemonCardComponent } from '../pokemon-card/pokemon-card.component';
-import { PokemonServiceService } from '../pokemon-service.service';
+import { PokemonServiceService } from '../pokemon-service/pokemon-service.service';
 import { PaginatorComponent } from '../paginator/paginator.component';
 import { Router } from '@angular/router';
-// import { JsonPipe } from '@angular/common';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-pokemon-list',
-  imports: [PokemonCardComponent, PaginatorComponent /* JsonPipe */],
+  imports: [PokemonCardComponent, PaginatorComponent, LoaderComponent],
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.scss',
-  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokemonListComponent {
-  /*   public pokemons = [
-      { name: 'Pikachu', src: 'https://img.pokemondb.net/artwork/large/pikachu.jpg', habilities: ['Electric', 'Thunder'] },
-      { name: 'Charmander', src: 'https://img.pokemondb.net/artwork/large/charmander.jpg', habilities: ['Fire', 'Flame'] },
-      { name: 'Squirtle', src: 'https://img.pokemondb.net/artwork/large/squirtle.jpg', habilities: ['Water', 'Bubble'] },
-      { name: 'Bulbasaur', src: 'https://img.pokemondb.net/artwork/large/bulbasaur.jpg', habilities: ['Grass', 'Poison'] }
-    ] */
-
-  // TODO: Cambiar el tipo de la variable pokemons a any[]
   public pokemons: any[] = [];
-
   pages?: any; // ? significa que es opcional y ! significa que no es null
-
   private pokemonService = inject(PokemonServiceService);
-
   private router = inject(Router);
+  loading: boolean = true;
 
   constructor() {
+    this.loading = true;
     this.pokemonService.getPokemonList().subscribe((data) => {
-      this.pokemons = data.results;
-      this.pages = { next: data.next, previous: data.previous };
-      // console.log('data', data.results);
+      setTimeout(() => {
+        this.pokemons = data.results;
+        this.pages = { next: data.next, previous: data.previous };
+        this.loading = false;
+      }, 500); 
     });
   }
 
@@ -49,21 +38,27 @@ export class PokemonListComponent {
   }
 
   nextPage() {
-    // console.log('Siguiente página');
     if (this.pages.next) {
+      this.loading = true;
       this.pokemonService.changePage(this.pages.next).subscribe((data) => {
-        this.pokemons = data.results;
-        this.pages = { next: data.next, previous: data.previous };
+        setTimeout(() => {
+          this.pokemons = data.results;
+          this.pages = { next: data.next, previous: data.previous };
+          this.loading = false;
+        }, 500);
       });
     }
   }
 
   prevPage() {
-    // console.log('Página anterior');
     if (this.pages.previous) {
+      this.loading = true;
       this.pokemonService.changePage(this.pages.previous).subscribe((data) => {
-        this.pokemons = data.results;
-        this.pages = { next: data.next, previous: data.previous };
+        setTimeout(() => {
+          this.pokemons = data.results;
+          this.pages = { next: data.next, previous: data.previous };
+          this.loading = false;
+        }, 500); 
       });
     }
   }
