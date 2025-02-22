f6769de4c07edc9193029f74de6f675aa5a94221
Samuel <samuciocan@gmail.com>
2025-02-22 18:49:26 +0100

    feat: enhance Pokemon component with loading state and improved layout

--- src/pokemon/pokemon.component.ts
+++ src/pokemon/pokemon.component.ts
@@ -1,32 +1,41 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PokemonServiceService } from '../pokemon-service.service';
-import { JsonPipe } from '@angular/common';
+import { PokemonServiceService } from '../pokemon-service/pokemon-service.service';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-pokemon',
-  imports: [JsonPipe],
+  standalone: true,
+  imports: [CommonModule, LoaderComponent],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.scss',
-  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokemonComponent {
   private router = inject(ActivatedRoute);
   private pokemonService = inject(PokemonServiceService);
   pokemon?: any; // Puede ser any o undefined
   pokemonName: string | null = null;
+  loading: boolean = true;
 
   constructor() {
-    // console.log('---->', this.router.snapshot.params['pokemonId']);
     this.pokemonName = this.router.snapshot.params['pokemonId'];
     this.getPokemonDetail(this.pokemonName);
   }
 
   getPokemonDetail(pokemonId: string | null): void {
     if (pokemonId) {
-      this.pokemonService.getPokemonDetail(pokemonId).subscribe((data) => {
-        this.pokemon = data;
-      });
+      this.loading = true;
+      setTimeout(() => {
+        this.pokemonService.getPokemonDetail(pokemonId).subscribe((data) => {
+          this.pokemon = data;
+          this.loading = false;
+        });
+      }, 500);
     }
+  }
+
+  capitalizeFirstLetter(name: string): string {
+    return name.charAt(0).toUpperCase() + name.slice(1);
   }
 }
